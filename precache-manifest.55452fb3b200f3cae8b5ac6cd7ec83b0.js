593c6c7b1970d87eb65c3a8bd9afee1d0f0e5524
jojomak13 <jojomak350@gmail.com>
2019-08-22 00:08:48 +0200

    Updates

---
+++ precache-manifest.55452fb3b200f3cae8b5ac6cd7ec83b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2980b99a51efd557e9643df17e059cdb",
+    "url": "/markdown-generator/index.html"
+  },
+  {
+    "revision": "d59b5982aa891cb74247",
+    "url": "/markdown-generator/static/css/main.1d9436cf.chunk.css"
+  },
+  {
+    "revision": "0dbd8801106c2072a36a",
+    "url": "/markdown-generator/static/js/2.6fd0a6ba.chunk.js"
+  },
+  {
+    "revision": "d59b5982aa891cb74247",
+    "url": "/markdown-generator/static/js/main.580a570f.chunk.js"
+  },
+  {
+    "revision": "ac9a3823b4c1610d23f3",
+    "url": "/markdown-generator/static/js/runtime~main.e6a64f08.js"
+  }
+]);
